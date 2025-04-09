41ed39acd9ed067c5f091554d539ce9764ce68c0
Yalemsew Mekuriaw <yalemone@gmail.com>
2025-04-09 12:00:06 -0500

    publisher dto

---
+++ src/main/java/com/example/libms/repository/BookRepository.java
@@ -0,0 +1,12 @@
+package com.example.libms.repository;
+
+import com.example.libms.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+     Optional<Book> findByIsbn(String isbn);
+     void deleteByIsbn(String isbn);
+        Optional<Book> findByTitle(String title);
+}
---
+++ src/main/java/com/example/libms/service/PublisherService.java
@@ -0,0 +1,18 @@
+package com.example.libms.service;
+
+import com.example.libms.model.Publisher;
+
+import java.util.Optional;
+
+public interface PublisherService {
+    //create
+    Optional<Publisher> createPublisher(Publisher publisher);
+
+
+    //find
+
+
+    //find all
+
+    //delete
+}
