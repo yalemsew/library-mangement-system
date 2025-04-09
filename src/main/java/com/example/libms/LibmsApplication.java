ff93a9b2325c386b5f9c9241900fca4101d0a8d0
Yalemsew Mekuriaw <yalemone@gmail.com>
2025-04-09 10:20:53 -0500

    project initilaization

---
+++ src/main/java/com/example/libms/LibmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.libms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibmsApplication.class, args);
+    }
+
+}
